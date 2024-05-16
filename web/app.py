3395c4fffbab5761a8f896315d02c9a38cf44699
hp0303 <89688200+hp0303@users.noreply.github.com>
2024-05-16 17:21:14 +0900

    Update app.py

--- web/app.py
+++ web/app.py
@@ -56,6 +56,8 @@ def upload_file():
             upload_file_drive(str(file_path))
             global file_id
 
+            os.remove(file_path)
+            
             return render_template('index.html', message='File uploaded successfully!', filename=f"<center>\n<img src=https://drive.google.com/uc?id={file_id} /><br>\n</center>")
 
     return render_template('index.html', message='File uploaded Failed')
